232e025e338ec4c30123f82017320c3b1ff02f03
Gabriel <gasouza@lcc-28.lcc.ufrn.br>
2019-02-20 10:44:00 -0300

    exercices

---
+++ exercices/ex01.py
@@ -0,0 +1,46 @@
+# @author Gabriel Araujo de Souza
+# @mail   gabriel_feg@hotmail.com
+# Resolucao da primeira lista de exercicios
+
+# (A)
+
+import cv2
+import numpy as np 
+
+# load image
+src = cv2.imread("img/img1.jpg")
+
+# separate channels
+b,g,r = cv2.split(src)
+
+# load 
+src_h, src_w, _ = src.shape
+
+# create matrix of zeros
+zeros = np.zeros((src_h, src_w), dtype= 'uint8')
+
+# merge channels
+img_blue = cv2.merge((b, zeros, zeros))
+img_green = cv2.merge((zeros, g, zeros))
+img_red = cv2.merge((zeros, zeros, r))
+
+# plot images
+cv2.imwrite("img/blue.png", img_blue) # blue channel
+cv2.imwrite("img/green.png", img_green) # green channel
+cv2.imwrite("img/red.png", img_red) # red channel
+
+
+# (B)
+
+image = cv2.imread("img/img1.jpg")
+horizontal_image = cv2.flip( image, 0)
+cv2.imwrite("img/img1_horizontal.png", horizontal_image)
+
+# (C)
+
+peixe1 = cv2.imread("img/peixe1.jpg")
+peixe2 = cv2.imread("img/peixe2.jpeg")	
+alpha = 0.5
+beta = (1.0 - alpha)
+dst = cv2.addWeighted(peixe1, alpha, peixe2, beta, 0.0)
+cv2.imwrite("img/peixeblending.png", dst)
